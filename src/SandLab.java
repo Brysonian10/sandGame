28047dd3410ca66ea46a049792a4984d7547048d
Bryson Spaulding <brysonian10@hotmail.com>
2019-04-09 09:23:06 -0600

    Got the sand and metal and empty space to show on canvas!

--- src/SandLab.java
+++ src/SandLab.java
@@ -7,6 +7,7 @@ public class SandLab
   //add constants for particle types here
   public static final int EMPTY = 0;
   public static final int METAL = 1;
+  public static final int SAND = 2;
   
   //do not add any more fields below
   private int[][] grid;
@@ -23,14 +24,15 @@ public class SandLab
     String[] names;
     // Change this value to add more buttons
     //Step 4,6
-    names = new String[2];
+    names = new String[3];
     // Each value needs a name for the button
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
+    names[SAND] = "Sand";
     
     //1. Add code to initialize the data member grid with same dimensions
     
-   grid = new int[numRows][numCols];
+   this.grid = new int[numRows][numCols];
     
     
     display = new SandDisplay("Falling Sand", numRows, numCols, names);
@@ -49,7 +51,23 @@ public class SandLab
   {
       //Step 3
    //Hint - use a nested for loop
-    
+    for(int rows = 0; rows < grid.length; rows++)
+    {
+    	for(int cols = 0; cols < grid[0].length; cols++)
+    	{
+    		int temp = grid[rows][cols];
+    		if(temp == 1)
+    		{
+    			display.setColor(rows, cols, Color.GRAY);
+    		} else if (temp == 0)
+    		{
+    			display.setColor(rows, cols, Color.BLACK);
+    		} else if(temp == 2)
+    		{
+    			display.setColor(rows, cols, Color.YELLOW);
+    		}
+    	}
+    }
   }
 
   //Step 5,7
